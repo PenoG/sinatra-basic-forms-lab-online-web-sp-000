ef91c3f2a1a29735033b6e413978e5c13181ea6f
Mason Phillips <mjphillips2000@gmail.com>
2021-01-13 20:40:20 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -3,12 +3,10 @@
 
 class Puppy
   attr_accessor :name, :breed, :age
-  DOGS= []
   
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
-    DOGS << self
   end
 end
